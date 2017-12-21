15fb6e9b8e4d8456722a39d7f51009a37779ca56
JohannDesobry <jo.desobry@gmail.com>
2017-12-21 21:47:03 +0100

    :sparkles: page results

--- src/js3/end.js
+++ src/js3/end.js
@@ -1,11 +1,55 @@
 const reset = document.querySelector('.device__solutionBtn--validate')
 
-reset.addEventListener('mouseover', function(e)
+if (reset)
 {
-    let parentOffset = reset.getBoundingClientRect(),
-    relX = e.clientX - parentOffset.left,
-    relY = e.clientY - parentOffset.top;
-    let span = document.querySelector('.device__solutionBtn--validate span')
-    span.style.top = relY + "px"
-    span.style.left = relX + "px"
-})
+    reset.addEventListener('mouseover', function(e)
+    {
+        let parentOffset = reset.getBoundingClientRect(),
+        relX = e.clientX - parentOffset.left,
+        relY = e.clientY - parentOffset.top;
+        let span = document.querySelector('.device__solutionBtn--validate span')
+        span.style.top = relY + "px"
+        span.style.left = relX + "px"
+    })
+}
+
+const day = document.querySelector('.device__day')
+
+if (day)
+{
+
+function removeDay ()
+{
+    setTimeout(() => {
+        day.style.opacity = 0
+    }, 1000);
+
+    setTimeout(() => {
+        day.style.display = 'none'
+    }, 1200);
+}
+
+removeDay()
+}
+
+/**
+ * 
+ * Results page
+ * 
+ */
+
+const shareStory = document.querySelector('.device__resultsContent--btn')
+
+if (shareStory)
+{
+    shareStory.addEventListener('mouseover', function(e)
+    {
+        let parentOffset = shareStory.getBoundingClientRect(),
+        relX = e.clientX - parentOffset.left,
+        relY = e.clientY - parentOffset.top;
+        let span = document.querySelector('.device__resultsContent--btn span')
+        span.style.top = relY + "px"
+        span.style.left = relX + "px"
+    })
+}
+ 
