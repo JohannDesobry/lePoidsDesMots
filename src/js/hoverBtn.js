3463e5ff2e3a42258f38d008d7b5dfd613dd95f8
JohannDesobry <jo.desobry@gmail.com>
2017-12-21 18:44:14 +0100

    :lipstick: ending page

--- src/js/hoverBtn.js
+++ src/js/hoverBtn.js
@@ -45,15 +45,3 @@ vibBox.addEventListener('mouseover', function(e)
     span.style.top = relY + "px"
     span.style.left = relX + "px"
 })
-
-const reset = document.querySelector('.device__solutionBtn--validate')
-
-reset.addEventListener('mouseover', function(e)
-{
-    let parentOffset = reset.getBoundingClientRect(),
-    relX = e.clientX - parentOffset.left,
-    relY = e.clientY - parentOffset.top;
-    let span = document.querySelector('.device__solutionBtn--validate span')
-    span.style.top = relY + "px"
-    span.style.left = relX + "px"
-})
